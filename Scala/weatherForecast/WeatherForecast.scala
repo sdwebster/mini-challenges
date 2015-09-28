52ce0a863f6faad7e29acc291e0db7ae4d586d14
Steve <stephendwebster@gmail.com>
2015-09-27 18:38:21 -0700

    Settle on xml over json for WeatherForecast

--- Scala/weatherForecast/WeatherForecast.scala
+++ Scala/weatherForecast/WeatherForecast.scala
@@ -3,8 +3,19 @@ import scala.util.parsing.json._
 
 
 object WeatherForecast extends App {
-  val url = "http://api.openweathermap.org/data/2.5/forecast?mode=json&lat==55&lon=0"
-  val l: List[String] = io.Source.fromURL(url).getLines.toList
-  val result = JSON.parseFull(l(0))
-  
+  // val url = "http://api.openweathermap.org/data/2.5/forecast?mode=json&lat==55&lon=0"
+  // val l: List[String] = io.Source.fromURL(url).getLines.toList
+  // val json:Option[Any] = JSON.parseFull(l(0))
+  // val map:Map[String,Any] = json.get.asInstanceOf[Map[String, Any]]
+  // val city = map.get("city")
+  // printf(city.get.asInstanceOf[String])
+  // for ((k,v) <- result) printf("key: %s, value: %s\n", k, v)
+  val url = "http://api.openweathermap.org/data/2.5/forecast?mode=xml&lat==55&lon=0"
+  val l: List[String] = io.Source.+fromURL(url).getLines.toList
+  val xml = l(0)
+  println(xml)
+
+  // def prettyPrintXML(s: String, indent: Int) = {
+    
+  // }
 }
